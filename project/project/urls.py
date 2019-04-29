1912c0eefcf8e9a2b682e38791602cf04d5eff7d
roger <rogerokello@gmail.com>
2019-04-29 22:09:56 +0300

    Configure URL routing

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
-from django.contrib import admin
+# from django.conf.urls import url
+# from django.contrib import admin
 
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
+
+from django.urls import path, include
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('', include('leads.urls')),
 ]
